4563064bf1214398504fb44677d6ff6fb10a8abd
Ellison Leão <ellisonleao@gmail.com>
2015-12-24 16:45:28 -0200

    add initial JSDoc. Resolve #6

--- sharer.js
+++ sharer.js
@@ -1,24 +1,38 @@
-/*
- * sharer.js
+/**
+ * Sharer.js
  *
- * Tiny js lib to create a simple share component. No deps.
- * Version: 0.2.1
- * Author: Ellison Leao
+ * @summary Tiny js lib to create a simple share component. No deps.
+ * @version: 0.2.1
+ * @author: Ellison Leao <ellisonleao@gmail.com>
  *
  */
 
 ;(function (window, document) {
     'use strict';
+    /**
+     * @constructor
+     */
     var Sharer = function(elem) {
         this.elem = elem;
     };
 
     Sharer.prototype = {
+        /**
+         *  @function getValue
+         *  @description Helper to get the attribute of a DOM element
+         *  @param {String} attr DOM element attribute
+         *  @returns {String|Empty} returns the attr value or empty string
+         */
         getValue: function(attr) {
             var val = this.elem.getAttribute(attr);
             return (val === undefined || val === null) ? '' : val;
         },
 
+        /**
+         * @event share
+         * @description Main share event. Will pop a window or redirect to a link
+         * based on the data-sharer attribute.
+         */
         share: function() {
             var sharer = this.elem.getAttribute('data-sharer');
             switch (sharer) {
@@ -83,7 +97,13 @@
                     break;
             }
         },
-
+        /**
+         * @event urlSharer
+         * @param {String} shareUrl
+         * @param {Object} params
+         * @param {Boolean} isLink - refers is the event will pop a new window or
+         * just redirect to #shareUrl
+         */
         urlSharer: function(shareUrl, params, isLink) {
             var params = typeof params === 'object' ? params : {},
                 keys = Object.keys(params),
@@ -102,7 +122,11 @@
                 window.location.href = shareUrl;
             }
         },
-
+        /**
+         * @event email
+         * @description Event to create a link to a mail client based on
+         * the data-* attributes
+         */
         email: function() {
             var to = this.getValue('data-to'),
                 subject = this.getValue('data-subject'),
@@ -112,6 +136,9 @@
         }
     }
 
+    /**
+     * Creates a click event on every DOM element which has the `sharer` class
+     */
     window.addEventListener('load', function() {
         var elems = document.querySelectorAll('.sharer'),
             i,
