8c4a79c0737c3c47788b89a2fc7a25db187f3151
Ellison Leão <ellisonleao@gmail.com>
2015-12-27 22:36:23 -0200

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'ellisonleao:sharerjs',
-    version: '0.2.2',
+    version: '0.2.3',
     summary: 'Create your own social share buttons',
     git: 'git://github.com/ellisonleao/sharer.js',
     documentation: 'README.md'
