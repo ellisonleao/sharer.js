6dcf53ebd992fbe09bc38d19873f13e8657f0f0a
Ellison Leão <ellisonleao@gmail.com>
2016-02-20 17:43:02 -0200

    bump version

--- sharer.js
+++ sharer.js
@@ -3,7 +3,7 @@
  * Sharer.js
  *
  * @description Create your own social share buttons
- * @version 0.2.12
+ * @version 0.2.13
  * @author Ellison Leao <ellisonleao@gmail.com>
  * @license GPLv3
  *
