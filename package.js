cd7cbbc407da6d5ebca7193707a9489a43d1c2a0
Ellison Leão <ellisonleao@gmail.com>
2016-06-24 12:12:16 -0300

    0.2.16

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'ellisonleao:sharerjs',
-    version: '0.2.15',
+    version: '0.2.16',
     summary: 'Create your own social share buttons',
     git: 'git://github.com/ellisonleao/sharer.js',
     documentation: 'README.md'
--- sharer.js
+++ sharer.js
@@ -3,7 +3,7 @@
  * Sharer.js
  *
  * @description Create your own social share buttons
- * @version 0.2.15
+ * @version 0.2.16
  * @author Ellison Leao <ellisonleao@gmail.com>
  * @license GPLv3
  *
